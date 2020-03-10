26d472f0f400e33a43d4815f4b9d46f363f885a7
spudmashmedia <johnnydong@spudmashmedia.com>
2020-03-10 14:21:19 +1100

    update snapshot for dropdown component

---
+++ components/Dropdown/Dropdown.test.js
@@ -0,0 +1,27 @@
+import React from "react";
+import Dropdown from "./Dropdown";
+import renderer from "react-test-renderer";
+
+describe("ErrorWidget Component Snapshot Test", () => {
+  test("renders correctly", () => {
+    const actionData = [
+      { id: "male", label: "Male" },
+      { id: "female", label: "Female" }
+    ];
+
+    const mockOnChangeHandler = jest.fn((e, newValue) => "male");
+
+    const tree = renderer
+      .create(
+        <Dropdown
+          name="test-dropdown"
+          label="Test Dropdown"
+          data={actionData}
+          onChangeHandler={mockOnChangeHandler}
+        />
+      )
+      .toJSON();
+
+    expect(tree).toMatchSnapshot();
+  });
+});
